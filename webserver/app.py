a7120371c945cf10c1fd700d853d7609a2fd04f0
Denis <d-kasakow@mail.de>
2023-06-27 18:55:15 +0200

    webserver: ungenutzte route GET / entfernt

--- webserver/app.py
+++ webserver/app.py
@@ -19,35 +19,6 @@ cursor = getDbConnection()
 weighting_setup(cursor)
 app = Flask(__name__)
 
-
-
-
-@app.route('/', methods=['GET'])
-def get_request():
-    question = request.args.get('question')
-    if question is None or len(question) < 1:
-        return jsonify("This is the server of nao.")
-    nlp = spacy.load("de_core_news_sm")
-    doc = nlp(question)
-    found_words = sentence_algorithm.sentence_detection(doc)
-    i = 0
-    while i < len(found_words):
-        found_words[i] = found_words[i].lower()
-        wd = db_connector.get_generic_term(found_words[i], cursor)
-        if wd is None:
-            i += 1
-            continue
-        found_words[i] = wd
-        i += 1
-    caseID = counter.count_ids(found_words, cursor)
-    if caseID is None:
-        return jsonify("Ich habe diese Frage nicht verstanden oder ich habe dazu leider keine Antwort.")
-    answer = db_connector.get_answer(caseID, cursor)
-    if answer is None:
-        return jsonify("-1")
-    return answer
-
-
 @app.route('/', methods=['POST'])
 def post_request():
 
--- webserver/sentence_algorithm.py
+++ webserver/sentence_algorithm.py
@@ -1,54 +1,38 @@
-import word
+from word import Word
 
 
 # Funktion erhält den erkannten Satz und verarbeitet diesen pro Wort in einer Schleife.
 # Danach wird das Wort genauestens untersucht. Am Ende wird der Originalsatz mit dem nun gekürzten Satz verglichen.
 def sentence_detection(sentence):
-    global token
-    global found_words
-    
     found_words = []
 
     for token in sentence:
-        check_word()
+        found_words = check_word(token, found_words)
     
     #Build list of words which can be returned
     words = []
-    for wd in found_words:
-        words.append(word.Word.get_lemma(wd))
+    for word in found_words:
+        words.append(word.Word.get_lemma(word))
     return words
 
 
 # Als erstes wird der POS untersucht. Wenn einer der Fälle eintritt, wird das Wort nicht weiter beachtet,
 # sondern in der Konsole mit einigen Daten ausgegeben. Kommt das Wort in keinen der Fälle,
 # wird es in einer weiteren Funktion auf den TAG überprüft.
-def check_word():
+def check_word(token, found_words):
     if not (token.pos_ == "AUX" or token.pos_ == "PUNCT" or token.pos_ == "PART"):
-        check_specific()
-    #else:
-    #    print("Rausgeflogen wegen POS:"
-    #          " \nText: " + token.text +
-    #          " \nLemma: " + token.lemma_ +
-    #          " \nPos: " + token.pos_ +
-    #          " \nTag: " + token.tag_ +
-    #          " \nDep: " + token.dep_ +
-    #          " \n-----------------------")
+        found_words = check_specific(token, found_words)
+    return found_words
 
 
 # Nach dem POS wird der TAG untersucht. Wenn einer der Fälle eintritt, wird das Wort nicht gespeichert,
 # sondern in der Konsole mit einigen Daten ausgegeben. Kommt das Wort in keinen der Fälle wird es zusammen mit
 # dem POS als Liste in die Liste "found_words" eingefügt.
-def check_specific():
+def check_specific(token, found_words):
     if not (token.tag_ == "PPER" or token.tag_ == "ART" or token.tag_ == "ADJD" or token.tag_ == "PROAV"
             or token.tag_ == "PRF" or token.tag_ == "PIS" or token.tag_ == "VAFIN" or token.tag_ == "PPOSAT"
             or token.tag_ == "PDS"):
-        new_word = word.Word(token.pos_, token.tag_, token.lemma_, token.dep_)
+        new_word = Word(token.pos_, token.tag_, token.lemma_, token.dep_)
         found_words.append(new_word)
-    #else:
-    #    print("Rausgeflogen wegen TAG:"
-    #          " \nText: " + token.text +
-    #          " \nLemma: " + token.lemma_ +
-    #         " \nPos: " + token.pos_ +
-    #          " \nTag: " + token.tag_ +
-    #          " \nDep: " + token.dep_ +
-    #          " \n-----------------------")
+
+    return found_words
